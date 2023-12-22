d24c2b85b5200e699c66d4e0b632a2d450fef59b
Denis Rezepov <me@dar920910.ru>
2023-12-22 13:26:04 +0300

    Created the TWEMP.Testing.GamingSupport.M2TW project template.

---
+++ TWEMP.Testing.GamingSupport.M2TW/src/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TWEMP.Testing.GamingSupport.M2TW;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
