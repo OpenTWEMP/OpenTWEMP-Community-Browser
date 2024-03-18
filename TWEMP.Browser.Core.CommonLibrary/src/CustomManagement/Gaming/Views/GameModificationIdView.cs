59fd01335dabffa8139aeaf6254406db10fb920e
Denis Rezepov <me@dar920910.ru>
2024-03-18 23:42:18 +0300

    Created the UpdatableGameModificationView class definition.

---
+++ TWEMP.Browser.Core.CommonLibrary/src/CustomManagement/Gaming/Views/GameModificationIdView.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="GameModificationIdView.cs" company="The OpenTWEMP Project">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TWEMP.Browser.Core.CommonLibrary.CustomManagement.Gaming.Views;
+
+/// <summary>
+/// Represents a game modification identifier which can be used as a unique key by more complex view entities.
+/// </summary>
+public record GameModificationIdView
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameModificationIdView"/> class.
+    /// </summary>
+    /// <param name="id">An object used by the game modification as the identifier.</param>
+    public GameModificationIdView(object? id)
+    {
+        this.NumericId = Convert.ToUInt32(id);
+        this.TextId = Convert.ToString(this.NumericId);
+    }
+
+    /// <summary>
+    /// Gets the identifier as a unsigned integer number.
+    /// </summary>
+    public uint NumericId { get; }
+
+    /// <summary>
+    /// Gets the identifier as a Unicode string.
+    /// </summary>
+    public string TextId { get; }
+}
---
+++ TWEMP.Browser.Core.CommonLibrary/src/CustomManagement/Gaming/Views/UpdatableGameModificationView.cs
@@ -0,0 +1,81 @@
+﻿// <copyright file="UpdatableGameModificationView.cs" company="The OpenTWEMP Project">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TWEMP.Browser.Core.CommonLibrary.CustomManagement.Gaming.Views;
+
+using TWEMP.Browser.Core.CommonLibrary.CustomManagement.Gaming.GameSupportPresets;
+
+/// <summary>
+/// Defines a game modification entity as a shared view which can be displayed via UI and updated on demand.
+/// </summary>
+public class UpdatableGameModificationView
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UpdatableGameModificationView"/> class.
+    /// </summary>
+    /// <param name="idView">The game modification identifier entity for this view.</param>
+    /// <param name="info">The game modification info entity for this view.</param>
+    private UpdatableGameModificationView(GameModificationIdView idView, GameModificationInfo info)
+    {
+        this.IdView = idView;
+        this.CurrentInfo = info;
+        this.ActivePreset = ModSupportPreset.CreateDefaultTemplate();
+    }
+
+    /// <summary>
+    /// Gets the game modification identifier entity for this view.
+    /// </summary>
+    public GameModificationIdView IdView { get; }
+
+    /// <summary>
+    /// Gets the current game modification info entity for this view.
+    /// </summary>
+    public GameModificationInfo CurrentInfo { get; }
+
+    /// <summary>
+    /// Gets the active mod support preset entity for this view.
+    /// </summary>
+    public ModSupportPreset ActivePreset { get; private set; }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="UpdatableGameModificationView"/> record
+    /// using an object of <see cref="RedistributableModPreset"/> class.
+    /// </summary>
+    /// <param name="idView">The game modification identifier entity for this view.</param>
+    /// <param name="info">The game modification info entity for this view.</param>
+    /// <param name="redistributablePreset">The redistributable preset for this game modification.</param>
+    /// <returns>A new configured instance of the <see cref="UpdatableGameModificationView"/> record.</returns>
+    public static UpdatableGameModificationView CreateGameModificationViewByRedistributablePreset(
+        GameModificationIdView idView,
+        GameModificationInfo info,
+        RedistributableModPreset redistributablePreset)
+    {
+        UpdatableGameModificationView gameModificationView = CreateGameModificationView(idView, info);
+        gameModificationView.ActivePreset = redistributablePreset.Data;
+        return gameModificationView;
+    }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="UpdatableGameModificationView"/> record
+    /// using an object of <see cref="CustomizableModPreset"/> class.
+    /// </summary>
+    /// <param name="idView">The game modification identifier entity for this view.</param>
+    /// <param name="info">The game modification info entity for this view.</param>
+    /// <param name="customizablePreset">The customizable preset for this game modification.</param>
+    /// <returns>A new configured instance of the <see cref="UpdatableGameModificationView"/> record.</returns>
+    public static UpdatableGameModificationView CreateGameModificationViewByCustomizablePreset(
+        GameModificationIdView idView,
+        GameModificationInfo info,
+        CustomizableModPreset customizablePreset)
+    {
+        UpdatableGameModificationView gameModificationView = CreateGameModificationView(idView, info);
+        gameModificationView.ActivePreset = customizablePreset.Data;
+        return gameModificationView;
+    }
+
+    private static UpdatableGameModificationView CreateGameModificationView(GameModificationIdView idView, GameModificationInfo info)
+    {
+        return new UpdatableGameModificationView(idView, info);
+    }
+}
