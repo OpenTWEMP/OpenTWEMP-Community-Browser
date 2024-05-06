627bcc357ec0d1dfffab9b785d2c7571310b2893
Denis Rezepov <me@dar920910.ru>
2024-05-06 13:24:59 +0300

    Prepared the GameConfigStateView record definition via composition of section state views records.

--- TWEMP.Browser.Core.GamingSupport/src/TotalWarEngine/M2TW/Configuration/Frontend/GameConfigStateView.cs
+++ TWEMP.Browser.Core.GamingSupport/src/TotalWarEngine/M2TW/Configuration/Frontend/GameConfigStateView.cs
@@ -2,14 +2,40 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
+#define CFG_STATE_PROPERTIES
+
 #pragma warning disable SA1600 // ElementsMustBeDocumented
 
 namespace TWEMP.Browser.Core.GamingSupport.TotalWarEngine.M2TW.Configuration.Frontend;
 
 using TWEMP.Browser.Core.GamingSupport.AbstractPlaceholders;
+using TWEMP.Browser.Core.GamingSupport.TotalWarEngine.M2TW.Configuration.Frontend.SectionStateViews;
 
 public record GameConfigStateView : ICustomConfigState
 {
+    public GameConfigStateView()
+    {
+    }
+
+    public ModSettingsSectionStateView? ModCoreSettingsSection { get; set; } // [features] + [io]
+
+    public ModDiagnosticSectionStateView? ModDiagnosticSection { get; set; } // [log]
+
+    public ModGameplaySectionStateView? ModGameplaySection { get; set; } // [game] + [misc]
+
+    public ModHotseatSectionStateView? HotseatSection { get; set; } // [hotseat] + [network] + [misc]
+
+    public GameAudioCfgSectionStateView? GameAudioCfgSection { get; set; } // [audio]
+
+    public GameCameraCfgSectionStateView? GameCameraCfgSection { get; set; } // [camera]
+
+    public GameControlsCfgSectionStateView? GameControlsCfgSection { get; set; } // [controls]
+
+    public GameUICfgSectionStateView? GameUICfgSection { get; set; } // [ui]
+
+    public GameVideoCfgSectionStateView? GameVideoCfgSection { get; set; } // [video]
+
+#if CFG_STATE_PROPERTIES
     public bool UseLauncherProvider_M2TWEOP { get; init; }
 
     public bool UseLauncherProvider_M2TWEOP_NativeSetup { get; init; }
@@ -39,6 +65,7 @@ public record GameConfigStateView : ICustomConfigState
     public bool IsShouldBeDeleted_SoundPacks { get; init; }
 
     public bool EnabledLogsHistorySaving { get; init; }
+#endif
 
     public ICfgOptSubSet[] RetrieveCurrentSettings()
     {
---
+++ TWEMP.Browser.Core.GamingSupport/src/TotalWarEngine/M2TW/Configuration/Frontend/SectionStateViews/ModDiagnosticSectionStateView.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="ModDiagnosticSectionStateView.cs" company="The OpenTWEMP Project">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+#pragma warning disable SA1600 // ElementsMustBeDocumented
+
+namespace TWEMP.Browser.Core.GamingSupport.TotalWarEngine.M2TW.Configuration.Frontend.SectionStateViews;
+
+public record ModDiagnosticSectionStateView
+{
+}
