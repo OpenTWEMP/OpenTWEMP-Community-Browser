8cd5c035f378f42d8f61ae38a8732efca47eaaf1
Denis Rezepov <me@dar920910.ru>
2024-05-10 08:05:10 +0300

    Implemented XML serialziation feature for the ModSubmodsConfiguration type.

--- TWEMP.Sandbox.TestApps/TWE-Submod-Switcher/Program.cs
+++ TWEMP.Sandbox.TestApps/TWE-Submod-Switcher/Program.cs
@@ -1,162 +1,191 @@
-﻿namespace TWE_Submod_Switcher
+﻿namespace TWE_Submod_Switcher;
+
+using System.Xml.Serialization;
+
+internal class Program
 {
-    internal class Program
+    const string LocaleID_RUS = "RUS";
+    const string LocaleID_ENG = "ENG";
+    const string LocaleID_ITA = "ITA";
+    const string LocaleID_SPA = "SPA";
+    const string LocaleID_DEU = "DEU";
+    const string LocaleID_FRA = "FRA";
+    const string LocaleID_POL = "POL";
+    const string LocaleID_CHI = "CHI";
+
+    static void Main(string[] args)
     {
-        const string LocaleID_RUS = "RUS";
-        const string LocaleID_ENG = "ENG";
-        const string LocaleID_ITA = "ITA";
-        const string LocaleID_SPA = "SPA";
-        const string LocaleID_DEU = "DEU";
-        const string LocaleID_FRA = "FRA";
-        const string LocaleID_POL = "POL";
-        const string LocaleID_CHI = "CHI";
+        Console.WriteLine("START");
 
-        static void Main(string[] args)
+        for (int argIndex = 0; argIndex < args.Length; argIndex++)
         {
-            Console.WriteLine("START");
-
-            for (int argIndex = 0; argIndex < args.Length; argIndex++)
-            {
-                Console.WriteLine($"arg[{argIndex}] = \"{args[argIndex]}\"");
-            }
-
-            Console.WriteLine();
-
-            foreach (var arg in args)
-            {
-                if (arg.Equals("--test"))
-                {
-                    ModSubmodsConfiguration configuration = CreateTestConfiguration();
-
-                    string homeDirectoryPath = Directory.GetCurrentDirectory();
-                    PrepareTestAssets(homeDirectoryPath, configuration);
-
-                    Console.WriteLine("RESULT: Prepared test assets.");
-                }
-
-                if (arg.Equals("--custom"))
-                {
-                    Console.WriteLine("1) Check the Game Mod Installation");
-                    Console.WriteLine("2) Detect All Config Files");
-                    Console.WriteLine("3) Read Data From Config Files");
-                    Console.WriteLine("4) Select a Custom Submod via User Input");
-                    Console.WriteLine("5) Clean Current Game Assets");
-                    Console.WriteLine("6) Copy Target Game Assets");
-                    Console.WriteLine("7) Display Result Information");
-                }
-            }
-
-            Console.WriteLine("FINISH");
+            Console.WriteLine($"arg[{argIndex}] = \"{args[argIndex]}\"");
         }
 
-        private static ModSubmodsConfiguration CreateTestConfiguration()
+        Console.WriteLine();
+
+        foreach (var arg in args)
         {
-            string workingDirectoryPath = Directory.GetCurrentDirectory();
-
-            List<ModGameLocale> supportedLocalizations = new ()
+            if (arg.Equals("--test"))
             {
-                new (id: LocaleID_RUS, title: "Russian", srcDirPath: LocaleID_RUS),
-                new (id: LocaleID_ENG, title: "English", srcDirPath: LocaleID_ENG),
-                new (id: LocaleID_ITA, title: "Italian", srcDirPath: LocaleID_ITA),
-                new (id: LocaleID_SPA, title: "Spanish", srcDirPath: LocaleID_SPA),
-                new (id: LocaleID_DEU, title: "German", srcDirPath: LocaleID_DEU),
-                new (id: LocaleID_FRA, title: "French", srcDirPath: LocaleID_FRA),
-                new (id: LocaleID_POL, title: "Polish", srcDirPath: LocaleID_POL),
-                new (id: LocaleID_CHI, title: "Chinese", srcDirPath: LocaleID_CHI),
-            };
+                string homeDirectoryPath = Directory.GetCurrentDirectory();
 
-            return  new ModSubmodsConfiguration(
-                srcDirPath: "submods", destDirPath: "data",
-                modGameLocales: supportedLocalizations.ToArray());
-        }
+                ModSubmodsConfiguration configuration = CreateTestConfiguration(homeDirectoryPath);
+                PrepareTestAssets(homeDirectoryPath, configuration);
 
-        private static void PrepareTestAssets(string homeDirectoryPath, ModSubmodsConfiguration configuration)
-        {
-            string submodsDirectoryPath = Path.Combine(
-                path1: homeDirectoryPath,
-                path2: configuration.SourceDirectoryPath);
-
-            if (!Directory.Exists(submodsDirectoryPath))
-            {
-                Directory.CreateDirectory(submodsDirectoryPath);
+                Console.WriteLine("RESULT: Prepared test assets.");
             }
 
-            CreateTestGameLocalizations(submodsDirectoryPath, configuration);
-        }
-
-        private static void CreateTestGameLocalizations(string gameLocalizationsDirectoryPath, ModSubmodsConfiguration configuration)
-        {
-            foreach (ModGameLocale localization in configuration.SupportedLocalizations)
+            if (arg.Equals("--custom"))
             {
-                string customLocaleDirectoryPath = Path.Combine(
-                    path1: gameLocalizationsDirectoryPath,
-                    path2: localization.SourceDirectoryPath);
-
-                CreateTestGameLocale(customLocaleDirectoryPath, localization);
+                Console.WriteLine("1) Check the Game Mod Installation");
+                Console.WriteLine("2) Detect All Config Files");
+                Console.WriteLine("3) Read Data From Config Files");
+                Console.WriteLine("4) Select a Custom Submod via User Input");
+                Console.WriteLine("5) Clean Current Game Assets");
+                Console.WriteLine("6) Copy Target Game Assets");
+                Console.WriteLine("7) Display Result Information");
             }
         }
 
-        private static void CreateTestGameLocale(string gameLocaleDirectoryPath, ModGameLocale gameLocale)
+        Console.WriteLine("FINISH");
+    }
+
+    private static ModSubmodsConfiguration CreateTestConfiguration(string directoryPath)
+    {
+        string workingDirectoryPath = Directory.GetCurrentDirectory();
+
+        List<ModGameLocale> supportedLocalizations = new ()
         {
-            const string testLocaleFileBaseName = "text_";
-            const string testLocaleFileExtension = ".txt";
-            const byte testLocaleFilesCount = 10;
+            new (id: LocaleID_RUS, title: "Russian", srcDirPath: LocaleID_RUS),
+            new (id: LocaleID_ENG, title: "English", srcDirPath: LocaleID_ENG),
+            new (id: LocaleID_ITA, title: "Italian", srcDirPath: LocaleID_ITA),
+            new (id: LocaleID_SPA, title: "Spanish", srcDirPath: LocaleID_SPA),
+            new (id: LocaleID_DEU, title: "German", srcDirPath: LocaleID_DEU),
+            new (id: LocaleID_FRA, title: "French", srcDirPath: LocaleID_FRA),
+            new (id: LocaleID_POL, title: "Polish", srcDirPath: LocaleID_POL),
+            new (id: LocaleID_CHI, title: "Chinese", srcDirPath: LocaleID_CHI),
+        };
 
-            if (!Directory.Exists(gameLocaleDirectoryPath))
-            {
-                Directory.CreateDirectory(gameLocaleDirectoryPath);
-            }
+        ModSubmodsConfiguration configuration = new (
+            srcDirPath: "submods", destDirPath: "data",
+            modGameLocales: supportedLocalizations.ToArray());
 
-            for (byte fileIndex = 0; fileIndex < testLocaleFilesCount; fileIndex++)
-            {
-                string customLocaleFileName = testLocaleFileBaseName
-                    + gameLocale.ID
-                    + "_" + fileIndex.ToString()
-                    + testLocaleFileExtension;
+        string configFilePath = configuration.GetConfigurationFilePath(directoryPath);
+        XmlSerializer serializer = new (configuration.GetType());
 
-                string customLocaleFilePath = Path.Combine(
-                    path1: gameLocaleDirectoryPath,
-                    path2: customLocaleFileName);
+        using (FileStream stream = File.Create(configFilePath))
+        {
+            serializer.Serialize(stream, configuration);
+        }
 
-                if (!File.Exists(customLocaleFilePath))
-                {
-                    File.Create(customLocaleFilePath);
-                }
-            }
+        return configuration;
+    }
+
+    private static void PrepareTestAssets(string directoryPath, ModSubmodsConfiguration configuration)
+    {
+        string submodsDirectoryPath = Path.Combine(
+            path1: directoryPath, path2: configuration.SourceDirectoryPath);
+
+        if (!Directory.Exists(submodsDirectoryPath))
+        {
+            Directory.CreateDirectory(submodsDirectoryPath);
+        }
+
+        CreateTestGameLocalizations(submodsDirectoryPath, configuration);
+    }
+
+    private static void CreateTestGameLocalizations(string gameLocalizationsDirectoryPath, ModSubmodsConfiguration configuration)
+    {
+        foreach (ModGameLocale localization in configuration.SupportedLocalizations)
+        {
+            string customLocaleDirectoryPath = Path.Combine(
+                path1: gameLocalizationsDirectoryPath,
+                path2: localization.SourceDirectoryPath);
+
+            CreateTestGameLocale(customLocaleDirectoryPath, localization);
         }
     }
 
-    record ModSubmodsConfiguration
+    private static void CreateTestGameLocale(string gameLocaleDirectoryPath, ModGameLocale gameLocale)
     {
-        public const string ConfigFileName = "LOCALE.json";
+        const string testLocaleFileBaseName = "text_";
+        const string testLocaleFileExtension = ".txt";
+        const byte testLocaleFilesCount = 10;
 
-        internal ModSubmodsConfiguration(string srcDirPath, string destDirPath, ModGameLocale[] modGameLocales)
+        if (!Directory.Exists(gameLocaleDirectoryPath))
         {
-            SourceDirectoryPath = srcDirPath;
-            DestinationDirectoryPath = destDirPath;
-            SupportedLocalizations = modGameLocales.ToArray();
+            Directory.CreateDirectory(gameLocaleDirectoryPath);
         }
 
-        internal string SourceDirectoryPath { get; set; }
-
-        internal string DestinationDirectoryPath { get; set; }
-
-        internal ModGameLocale[] SupportedLocalizations { get; set; }
-    }
-
-    internal record ModGameLocale
-    {
-        internal ModGameLocale(string id, string title, string srcDirPath)
+        for (byte fileIndex = 0; fileIndex < testLocaleFilesCount; fileIndex++)
         {
-            ID = id;
-            Title = title;
-            SourceDirectoryPath = srcDirPath;
+            string customLocaleFileName = testLocaleFileBaseName
+                + gameLocale.ID
+                + "_" + fileIndex.ToString()
+                + testLocaleFileExtension;
+
+            string customLocaleFilePath = Path.Combine(
+                path1: gameLocaleDirectoryPath,
+                path2: customLocaleFileName);
+
+            if (!File.Exists(customLocaleFilePath))
+            {
+                File.Create(customLocaleFilePath);
+            }
         }
-
-        internal string ID { get; set; }
-
-        internal string Title { get; set; }
-
-        internal string SourceDirectoryPath { get; set; }
     }
+}
+
+public record ModSubmodsConfiguration
+{
+    public const string ConfigFileName = "configuration.xml";
+
+    public ModSubmodsConfiguration()
+    {
+    }
+
+    public ModSubmodsConfiguration(string srcDirPath, string destDirPath, ModGameLocale[] modGameLocales)
+    {
+        SourceDirectoryPath = srcDirPath;
+        DestinationDirectoryPath = destDirPath;
+        SupportedLocalizations = modGameLocales.ToArray();
+    }
+
+    [XmlAttribute("SourceDirectoryRelativePath")]
+    public string SourceDirectoryPath { get; set; }
+
+    [XmlAttribute("DestinationDirectoryRelativePath")]
+    public string DestinationDirectoryPath { get; set; }
+
+    [XmlElement("Localization")]
+    public ModGameLocale[] SupportedLocalizations { get; set; }
+
+    public string GetConfigurationFilePath(string directoryPath)
+    {
+        return Path.Combine(directoryPath, ConfigFileName);
+    }
+}
+
+public record ModGameLocale
+{
+    public ModGameLocale()
+    {
+    }
+
+    public ModGameLocale(string id, string title, string srcDirPath)
+    {
+        ID = id;
+        Title = title;
+        SourceDirectoryPath = srcDirPath;
+    }
+
+    [XmlAttribute("ID")]
+    public string ID { get; set; }
+
+    [XmlAttribute("Title")]
+    public string Title { get; set; }
+
+    [XmlAttribute("SourceDirectoryPath")]
+    public string SourceDirectoryPath { get; set; }
 }
