83213cc790ba903a48a88e4b765bbd4cefce76ca
Denis Rezepov <me@dar920910.ru>
2024-09-06 13:25:03 +0300

    Added a temp duplicate of the IGameLauncherAgent interface type to simplify code migration.

---
+++ TWEMP.Browser.Core.GamingSupport/src/AbstractPlaceholders/IGameLauncherAgent.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IGameLauncherAgent.cs" company="The OpenTWEMP Project">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+#pragma warning disable SA1600 // ElementsMustBeDocumented
+
+namespace TWEMP.Browser.Core.GamingSupport.AbstractPlaceholders;
+
+// TODO: Replace to the imported type - TWEMP.Browser.Core.CommonLibrary.GameLauncherFeatures.IGameLauncherAgent.
+public interface IGameLauncherAgent
+{
+    /// <summary>
+    /// Launches a game according to the rules of its game launcher agent.
+    /// </summary>
+    public abstract void Execute();
+}
