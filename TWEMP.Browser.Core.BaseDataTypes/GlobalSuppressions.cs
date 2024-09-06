6747dab813e09c66fb2e9449d1a6157a5f85d670
Denis Rezepov <me@dar920910.ru>
2024-09-06 11:05:12 +0300

    Prepared the initial TWEMP.Browser.Core.BaseDataTypes class library. Added the GameEngineSupportType enumeration type definition.

---
+++ TWEMP.Browser.Core.BaseDataTypes/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="The OpenTWEMP Project">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+/* This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to this project.
+ * Project-level suppressions either have no target or are given a specific target and scoped to
+ * a namespace, type, member, etc. */
+
+using System.Diagnostics.CodeAnalysis;
+
+#pragma warning disable SA1404 // Code analysis suppression should have justification
+
+[assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "<Pending>", Scope = "namespace", Target = "~N:TWEMP.Browser.Core.BaseDataTypes")]
