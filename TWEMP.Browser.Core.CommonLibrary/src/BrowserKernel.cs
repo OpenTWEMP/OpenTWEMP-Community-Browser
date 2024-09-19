9c3e79eeae7c0f1da9423cbd26684027f7becb5b
Denis Rezepov <me@dar920910.ru>
2024-09-19 17:14:42 +0300

    Created the initial BrowserKernel class.

---
+++ TWEMP.Browser.Core.CommonLibrary/src/BrowserKernel.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="BrowserKernel.cs" company="The OpenTWEMP Project">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TWEMP.Browser.Core.CommonLibrary;
+
+/// <summary>
+/// Defines the kernel of any OpenTWEMP Community Browser implementation.
+/// </summary>
+public class BrowserKernel
+{
+}
