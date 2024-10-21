c6a3e3b888ab4e22084bbc005f08fcb7ef17b34a
Denis Rezepov <me@dar920910.ru>
2024-10-21 19:14:34 +0300

    Implemented the GameVideoCfgSectionStateView record definition.

--- TWEMP.Browser.Core.GamingSupport/src/TotalWarEngine/M2TW/Configuration/Backend/GameConfigOptionsVocabulary.cs
+++ TWEMP.Browser.Core.GamingSupport/src/TotalWarEngine/M2TW/Configuration/Backend/GameConfigOptionsVocabulary.cs
@@ -12,48 +12,4 @@ public static class GameConfigOptionsVocabulary
     public const string LogConfigSwitch = "log";
 
     public const string LogTo = "to";
-
-    // [video]
-    public const string VideoConfigSwitch = "video";
-
-    public const string VideoAnisotropicLevel = "anisotropic_level";
-    public const string VideoAntiAliasMode = "anti_alias_mode";
-    public const string VideoAntialiasing = "antialiasing";
-    public const string VideoAssassinationMovies = "assassination_movies";
-    public const string VideoAutodetect = "autodetect";
-    public const string VideoBattleResolution = "battle_resolution";
-    public const string VideoBloom = "bloom";
-    public const string VideoBuildingDetail = "building_detail";
-    public const string VideoCampaignResolution = "campaign_resolution";
-    public const string VideoDepthShadows = "depth_shadows";
-    public const string VideoDepthShadowsResolution = "depth_shadows_resolution";
-    public const string VideoEffectQuality = "effect_quality";
-    public const string VideoEventMovies = "event_movies";
-    public const string VideoGamma = "gamma";
-    public const string VideoGrassDistance = "grass_distance";
-    public const string VideoGroundBuffersPerNode = "ground_buffers_per_node";
-    public const string VideoGroundCoverBuffersPerNode = "ground_cover_buffers_per_node";
-    public const string VideoInfiltrationMovies = "infiltration_movies";
-    public const string VideoMovies = "movies";
-    public const string VideoModelBuffersPerNode = "model_buffers_per_node";
-    public const string VideoNoBackgroundFmv = "no_background_fmv";
-    public const string VideoReflection = "reflection";
-    public const string VideoSabotageMovies = "sabotage_movies";
-    public const string VideoShader = "shader";
-    public const string VideoShowBanners = "show_banners";
-    public const string VideoShowPackageLitter = "show_package_litter";
-    public const string VideoSkipMipLevels = "skip_mip_levels";
-    public const string VideoSplashes = "splashes";
-    public const string VideoSpriteBuffersPerNode = "sprite_buffers_per_node";
-    public const string VideoStencilShadows = "stencil_shadows";
-    public const string VideoSubtitles = "subtitles";
-    public const string VideoTerrainQuality = "terrain_quality";
-    public const string VideoTextureFiltering = "texture_filtering";
-    public const string VideoUnitDetail = "unit_detail";
-    public const string VideoVegetation = "vegetation";
-    public const string VideoVegetationQuality = "vegetation_quality";
-    public const string VideoVsync = "vsync";
-    public const string VideoWaterBuffersPerNode = "water_buffers_per_node";
-    public const string VideoWidescreen = "widescreen";
-    public const string VideoWindowed = "windowed";
 }
