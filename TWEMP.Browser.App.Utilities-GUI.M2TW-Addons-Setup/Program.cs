13e5e0879fa2a9a73f72d55f3f0eeef8e501fbf4
Denis Rezepov <me@dar920910.ru>
2024-10-25 12:25:07 +0300

    Prepared an initial implementation of the "TWEMP.Browser.App.Utilities-GUI.M2TW-Addons-Setup" extension app.

--- TWEMP.Browser.App.Utilities-GUI.M2TW-Addons-Setup/Program.cs
+++ TWEMP.Browser.App.Utilities-GUI.M2TW-Addons-Setup/Program.cs
@@ -1,17 +1,21 @@
-namespace TWEMP.Browser.App.Utilities_GUI.M2TW_Addons_Setup
+// <copyright file="Program.cs" company="The OpenTWEMP Project">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TWEMP.Browser.App.Utilities_GUI.M2TW_Addons_Setup;
+
+/// <summary>
+///  The root class of the application.
+/// </summary>
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    internal static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-        }
+        ApplicationConfiguration.Initialize();
+        Application.Run(new TwempExtensionStartupForm());
     }
 }
---
+++ TWEMP.Browser.App.Utilities-GUI.M2TW-Addons-Setup/TwempExtensionStartupForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="TwempExtensionStartupForm.Designer.cs" company="The OpenTWEMP Project">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TWEMP.Browser.App.Utilities_GUI.M2TW_Addons_Setup
+{
+    partial class TwempExtensionStartupForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            startupButton = new Button();
+            SuspendLayout();
+            // 
+            // startupButton
+            // 
+            startupButton.Location = new Point(12, 12);
+            startupButton.Name = "startupButton";
+            startupButton.Size = new Size(225, 126);
+            startupButton.TabIndex = 0;
+            startupButton.Text = "LAUNCH";
+            startupButton.UseVisualStyleBackColor = true;
+            startupButton.Click += StartupButton_Click;
+            // 
+            // TwempExtensionStartupForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(249, 150);
+            Controls.Add(startupButton);
+            Name = "TwempExtensionStartupForm";
+            Text = "M2TW Addons Setup";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button startupButton;
+    }
+}
